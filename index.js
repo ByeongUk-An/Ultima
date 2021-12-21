2e7c47850f052acc0b29561cd55d4ef581ad77a8
An ByeongUk <quddnr005@naver.com>
2021-12-22 00:10:27 +0900

    header function modify

--- index.js
+++ index.js
@@ -29,3 +29,63 @@ var swiper = new Swiper('.mySwiper2', {
     prevEl: '.swiper-button-prev',
   },
 });
+
+$(document).ready(function () {
+  for (let i = 0; i < $('.drop-down-item').length; i++) {
+    $('.drop-down-item')
+      .eq(i)
+      .hover(function () {
+        $('.drop-down-img').eq(i).addClass('active');
+      });
+  }
+  for (let j = 0; j < $('.drop-down-item').length; j++) {
+    $('.drop-down-item')
+      .eq(j)
+      .mouseleave(function () {
+        $('.drop-down-img').eq(j).removeClass('active');
+      });
+  }
+
+  $('.hambuger').click(function () {
+    // $('.drop-down').toggleClass('active');
+    $('.drop-down').slideToggle();
+    $(this).css('display', 'none');
+    $('.close').css('display', 'block');
+  });
+  $('.close').click(function () {
+    $('.drop-down').slideToggle();
+    // $('.drop-down').toggleClass('active');
+    $(this).css('display', 'none');
+    $('.hambuger').css('display', 'block');
+  });
+
+  $(window).scroll(function () {
+    if ($('.hambuger').css('display') === 'block') {
+      // console.log(headerOffset.top);
+      // console.log($(window).scrollTop(), 'window');
+      if ($(window).scrollTop() > 110) {
+        $('.header-wrap').addClass('active');
+      } else {
+        $('.header-wrap').removeClass('active');
+      }
+    }
+  });
+});
+
+// if ($(window).scrollTop() > headerOffset.top) {
+//   $('.header-wrap').css('background', '#fff');
+//   $('.hambuger').css('color', '#000');
+//   $('.lang').css('color', '#000');
+//   $('.logo-svg').css('fill', '#000');
+//   $('.enquire-btn').css('border', '1px solid #000');
+//   $('.enquire-text').css('color', '#4a4a49');
+//   $('.errow-path').css('stroke', '#4a4a49');
+// } else {
+//   $('.header-wrap').css('background', 'transparent');
+//   $('.hambuger').css('color', '#fff');
+//   $('.lang').css('color', '#fff');
+//   $('.logo-svg').css('fill', '#fff');
+//   $('.enquire-text').css('color', '#fff');
+//   $('.enquire-btn').css('border', '1px solid #fff');
+//   $('.errow-path').css('stroke', '#fff');
+// }
